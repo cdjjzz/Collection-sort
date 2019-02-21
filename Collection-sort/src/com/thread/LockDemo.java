3d92c50f634fb9ea3fbba08d81935057fe3bc5a0
cdjjzz <1813604115@qq.com>
2019-02-22 00:00:59 +0800

    777

---
+++ Collection-sort/src/com/thread/LockDemo.java
@@ -0,0 +1,46 @@
+package com.thread;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockDemo {
+    private static ReentrantLock lock = new ReentrantLock();
+    static  Condition condition=lock.newCondition();
+    public static void main(String[] args) {
+    	final In in=new  In();
+        for (int i = 0; i < 5; i++) {
+            Thread thread = new Thread(() -> {
+                lock.lock();
+                int j=in.getI();
+                in.setI(++j);
+                try {
+                	System.out.println(in.getI());
+                	if(in.getI()==0){
+                		condition.await();
+                	}else{
+                		condition.signal();
+                		System.out.println(Thread.currentThread().getName());
+                	}
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            });
+            thread.start();
+        }
+    }
+    static class In{
+    	volatile int i;
+
+		public int getI() {
+			return i;
+		}
+
+		public void setI(int i) {
+			this.i = i;
+		}
+    	
+    	
+    }
+}
