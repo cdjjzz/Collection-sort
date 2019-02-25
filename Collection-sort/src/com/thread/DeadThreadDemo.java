c2664e071c146e47da438787f53ffa508c29efbd
fcb <1813604115@qq.com>
2019-02-25 22:46:40 +0800

    44

--- Collection-sort/src/com/thread/DeadThreadDemo.java
+++ Collection-sort/src/com/thread/DeadThreadDemo.java
@@ -45,7 +45,7 @@ public class DeadThreadDemo {
 
 			System.out.println("555");
 			System.out.println("55588");
-			System.out.println("222");
+			System.out.println("222333");
 
                     }
                 }
