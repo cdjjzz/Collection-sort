87508f894306f8f58750b4e4ef1c0e616b52af1e
cdjjzz <1813604115@qq.com>
2019-02-25 21:10:09 +0800

    13213123

--- Collection-sort/src/com/thread/DeadThreadDemo.java
+++ Collection-sort/src/com/thread/DeadThreadDemo.java
@@ -1,12 +1,10 @@
 package com.thread;
 
 /**
- * 演示线程死锁
  * @author lsf53_000
  *
  */
 public class DeadThreadDemo {
-	//类变量
 	private static String resource_a = "A";
     private static String resource_b = "B";
 
@@ -39,8 +37,7 @@ public class DeadThreadDemo {
                     synchronized (resource_a) {
                         System.out.println("get resource a");
                         System.out.println("1111232341231232341");
-                        //final 允许 引用对象不允许地址，但能修改里面的非final属性的值
-                        //final 允许重载，不允许重写
+ 
                     }
                 }
             }
