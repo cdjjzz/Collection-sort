4d10c183cef22da07b518e48d40283981f471049
cdjjzz <1813604115@qq.com>
2019-03-24 00:40:08 +0800

    123123123

--- Collection-sort/src/com/thread/Test.java
+++ Collection-sort/src/com/thread/Test.java
@@ -7,14 +7,12 @@ public class Test {
 		   long a1 = System.currentTimeMillis();
 		   for (int i = 0; i < 100000; i++) {
 		       alloc();
-		       1232
-		       123123
 		   }
 		   // 查看执行时间
 		   long a2 = System.currentTimeMillis();
 		   System.out.println("cost " + (a2 - a1) + " ms");
 		   // 为了方便查看堆内存中对象个数，线程sleep
-		   new ArrayList<>().forEach(action);
+		 //  new ArrayList<>().forEach(action);
 		   try {
 		       Thread.sleep(100000);
 		   } catch (InterruptedException e1) {
